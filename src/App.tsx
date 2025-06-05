8f56a19ecfc8976669d59bc71eff7200649eb7de
Ivan Hurtado <ivan.hurtado92@outlook.com>
2025-06-05 14:35:30 -0600

    Background color added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { BooksPage } from "./components/BooksPage/BooksPage";
+import "./App.css"
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
+    <div className="min-h-screen p-6 main">
       <h1 className="text-3xl font-bold mb-6 text-center">Books Library</h1>
       <BooksPage />
     </div>
